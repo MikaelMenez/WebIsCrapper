34910311715c1a8b395f769668dff8eb11ee9413
kaio15922 <kaiorenato12345@gmail.com>
2026-02-24 22:33:43 -0300

    Adicionando a Regex para leitura da URL da wiki e pedindo entrada do usuario

--- webisCrapper.c
+++ webisCrapper.c
@@ -4,6 +4,7 @@
 #include <curl/curl.h>
 #include <stdlib.h>
 #include <string.h>
+#include <locale.h>
 typedef struct {
     char * content;
     size_t tam;
@@ -24,21 +25,46 @@ int main(void){
     resposta buffer;
     buffer.content=malloc(1);
     buffer.tam=0;
-    if (curl){
-        CURLcode result;
+    setlocale(LC_ALL, ""); //Locale para permitir acentos
+    //Iniciando compilacao da regex e pegada da url do user. Tambem verificando match.
+    regex_t url_wiki;
+    char url_user[90];
+    char padraoDaWiki[] = "^https?:\\/\\/[a-z]{2}\\.wikipedia\\.org\\/wiki\\/[[:alnum:]_%]+$";
 
-        curl_easy_setopt(curl, CURLOPT_USERAGENT, "Bot_aprendizado_web_scrapper/1.0 (mixaelmenezes@gmail.com)");
-        curl_easy_setopt(curl, CURLOPT_ACCEPT_ENCODING,"");
-        curl_easy_setopt(curl, CURLOPT_URL, "https://pt.wikipedia.org/wiki/Ben_10");
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION , save_on_memory);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void*)&buffer);
-        result=curl_easy_perform(curl);
-        if (result==CURLE_OK){
-            printf("esse é o html :%s",buffer.content);
-        }
-        curl_easy_cleanup(curl);
-
+    if(regcomp(&url_wiki, padraoDaWiki, REG_EXTENDED) != 0)
+    {
+        printf("Nao foi possivel compilar a regex\n");
+        return 1;
     }
-    free(buffer.content);
-    return 0;
+
+    printf("Copie aqui a URL da Wikipedia que voce deseja acessar: ");
+    scanf("%s", url_user);
+
+    if(regexec(&url_wiki, url_user, 0, NULL, 0) == 0)
+    {
+        if (curl){
+            CURLcode result;
+
+            curl_easy_setopt(curl, CURLOPT_USERAGENT, "Bot_aprendizado_web_scrapper/1.0 (mixaelmenezes@gmail.com)");
+            curl_easy_setopt(curl, CURLOPT_ACCEPT_ENCODING,"");
+            curl_easy_setopt(curl, CURLOPT_URL, url_user);
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION , save_on_memory);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void*)&buffer);
+            result=curl_easy_perform(curl);
+            if (result==CURLE_OK){
+                printf("esse é o html :%s",buffer.content);
+            }
+            curl_easy_cleanup(curl);
+
+        }
+        free(buffer.content);
+        regfree(&url_wiki);
+        return 0;
+    }
+    else
+    {
+        printf("Texto invalido\n");
+        regfree(&url_wiki);
+        return 1;
+    }
 }
